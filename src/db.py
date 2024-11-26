8967a2d813c6d32335a5054d8978c516d9be428a
erik-mauricio <em882@cornell.edu>
2024-11-26 17:56:49 -0500

    Created users table for class User and add serializations

--- src/db.py
+++ src/db.py
@@ -2,10 +2,48 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Group(db.Model):
-    pass
-
 class User(db.Model):
+    """
+    User Model 
+    """
+
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    netid = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, **kwargs):
+        """
+        Initializes a user object
+        """
+        
+        self.name = kwargs.get("name")
+        self.netid = kwargs.get("netid")
+
+    def serialize(self):
+        """
+        Serializes a user object
+        """
+        
+        return {
+                "id": self.id,
+                "name": self.name,
+                "netid": self.netid
+                }
+
+    def simple_serialize(self):
+        """
+        Simple Serializes a user object
+        """
+
+        return {
+                "id": self.id,
+                "name": self.name,
+                "netid": self.netid,
+                }
+    
+
+class Group(db.Model):
     pass
 
 class Rate(db.Model):
