50aaea747c8a4121fd7cd51c1ac1ebc8fc38e97a
erik-mauricio <em882@cornell.edu>
2024-11-26 19:28:27 -0500

    Set up group to user - one to many relationship

--- src/db.py
+++ src/db.py
@@ -2,6 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+Rating_To_Student = db.Table(
+                    "student_ratings", db.Model.metadata,
+                    db.Column("user_id", db.Integer, db.ForeignKey("users.id"))
+                    )
+
+Rating_To_Group = db.Table(
+                    "group_ratings", db.Model.metadata,
+                    db.Column("group_id", db.Integer, db.ForeignKey("groups.id"))
+                    )
+
 class User(db.Model):
     """
     User Model 
@@ -11,6 +21,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     netid = db.Column(db.Integer, nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey("groups.id"), nullable=False)
 
     def __init__(self, **kwargs):
         """
@@ -44,7 +55,73 @@ class User(db.Model):
     
 
 class Group(db.Model):
-    pass
+    """
+    Group Model
+    """
+
+    __tablename__ = "groups"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    users = db.relationship("User", cascade="delete")
+
+    def __init__(self, **kwargs):
+        """
+        Initializes group object/entry
+        """
+        self.name= kwargs.get("name")
+        self.users = kwargs.get("users")
+   
+    def serialize(self):
+        """
+        Serializes a group object
+        """
+    
+        return {
+            "id": self.id,
+            "name": self.name,
+            "users": [u.serialize() for u in self.users]
+               }
+
+    def simple_serialize(self):
+        """
+        Simple serializes a group object
+        """
+
+        return {
+                "id": self.id,
+                "name": self.name
+                }
 
 class Rate(db.Model):
-    pass
+
+    __tablename__ = "rating"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    stars = db.Column(db.Integer, nullable=False)
+
+
+    def __init__(self, **kwargs):
+        """
+        Initializes an rate object
+        """
+
+        self.stars = kwargs.get("stars")
+
+    def serialize(self):
+        """
+        Serializes rate object
+        """
+
+        return {
+                "id": self.id,
+                "stars": self.stars,
+                }
+
+    def simple_serialize(self):
+        """
+        Simple Serializes a rate object
+        """
+
+        return {
+                "id": self.id,
+                "stars": self.stars,
+                }
